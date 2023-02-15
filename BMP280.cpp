7896b683951ac5540fb84a2d393cc466eef6f642
Satoshi Ohya <conatus.11099@gmail.com>
2023-02-15 12:15:08 +0900

    first commit

---
+++ BMP280.cpp
@@ -0,0 +1,9 @@
+/*
+ * BMP280.cpp
+ *
+ *  Created on: Feb 15, 2023
+ *      Author: OHYA Satoshi
+ */
+
+#include "BMP280.h"
+
---
+++ BMP280.h
@@ -0,0 +1,14 @@
+/*
+ * BMP280.h
+ *
+ *  Created on: Feb 15, 2023
+ *      Author: OHYA Satoshi
+ */
+
+#ifndef BMP280_BMP280_H_
+#define BMP280_BMP280_H_
+
+class BMP280 {
+};
+
+#endif /* BMP280_BMP280_H_ */
